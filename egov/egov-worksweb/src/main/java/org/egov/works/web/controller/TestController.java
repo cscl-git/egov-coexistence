c184de18ec90c1e843ad6fef139a8cf86a7d9b1c
Ubuntu <rajatp932@egovdev.emkim0y3kg2uzeulqk2f3yusyc.ix.internal.cloudapp.net>
2020-07-07 14:19:16 +0530

    works setup

--- egov/egov-worksweb/src/main/java/org/egov/works/web/controller/TestController.java
+++ egov/egov-worksweb/src/main/java/org/egov/works/web/controller/TestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class TestController {
 	
 	
-	@RequestMapping(value = "/create}", method = RequestMethod.GET)
+	@RequestMapping(value = "/create}", method = RequestMethod.POST)
 	public String showNewFormGet(final Model model,
 			HttpServletRequest request) {
 		
