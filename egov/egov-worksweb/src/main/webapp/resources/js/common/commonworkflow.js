56f917303e8ad37e99f8bebeaaf3d9f251c596ef
Ubuntu <rajatp932@egovdev.emkim0y3kg2uzeulqk2f3yusyc.ix.internal.cloudapp.net>
2020-09-02 16:56:51 +0530

    Works changes workflow

--- egov/egov-worksweb/src/main/webapp/resources/js/common/commonworkflow.js
+++ egov/egov-worksweb/src/main/webapp/resources/js/common/commonworkflow.js
@@ -47,9 +47,10 @@
  */
 $(document).ready(function()
 {	
+	console.log("workflow file got updated................");
 	$('#approvalDepartment').change(function(){
 		$.ajax({
-			url: "/eis/ajaxWorkFlow-getDesignationsByObjectType",     
+			url: "/services/eis/ajaxWorkFlow-getDesignationsByObjectType",     
 			type: "GET",
 			data: {
 				approvalDepartment : $('#approvalDepartment').val(),
@@ -66,7 +67,31 @@ $(document).ready(function()
 				$('#approvalDesignation').empty();
 				$('#approvalDesignation').append($("<option value=''>Select from below</option>"));
 				$.each(response, function(index, value) {
-					$('#approvalDesignation').append($('<option>').text(value.name).attr('value', value.id));
+					$('#approvalDesignation').append($('<option>').text(value.name).attr('value', value.code));
+				});
+				$('#approvalDesignation').val($('#approvalDesignationValue').val());
+				$('#approvalDesignation').trigger('change');
+			}, 
+			error: function (response) {
+				//bootbox.alert('json fail');
+				console.log("failed");
+			}
+		});
+	});
+	
+	/*$('#approvalDepartment').change(function(){
+		
+		$.ajax({
+			url: "/services/EGF/designations",     
+			type:'GET',
+			contentType:'application/json',
+			//data:JSON.stringify(jsonData),
+			success: function (response) {
+				console.log("success"+response);
+				$('#approvalDesignation').empty();
+				$('#approvalDesignation').append($("<option value=''>Select from below</option>"));
+				$.each(response, function(index, value) {
+					$('#approvalDesignation').append($('<option>').text(value.name).attr('value', value.code));
 				});
 				$('#approvalDesignation').val($('#approvalDesignationValue').val());
 				$('#approvalDesignation').trigger('change');
@@ -76,23 +101,30 @@ $(document).ready(function()
 				console.log("failed");
 			}
 		});
-	});
+	});*/
+	
+
 	
 	$('#approvalDesignation').change(function(){
+		
+		
+				todayDate = new Date(Date.now()).toLocaleString(),
+				
+				designationId=  $('#approvalDesignation').val(),
+				departmentId =$('#approvalDepartment').val();
+		if(departmentId && designationId)
 		$.ajax({
-			url: "/eis/ajaxWorkFlow-positionsByDepartmentAndDesignation",     
+			url: "/services/EGF/approvers/"+departmentId+"/"+designationId,     
 			type: "GET",
-			data: {
-				approvalDesignation : $('#approvalDesignation').val(),
-				approvalDepartment : $('#approvalDepartment').val()    
-			},
-			dataType: "json",
+			contentType:'application/json',
+//			data: JSON.stringify(jsonData),
 			success: function (response) {
 				console.log("success"+response);
 				$('#approvalPosition').empty();
 				$('#approvalPosition').append($("<option value=''>Select from below</option>"));
 				$.each(response, function(index, value) {
-					$('#approvalPosition').append($('<option>').text(value.userName+'/'+value.positionName).attr('value', value.positionId));  
+					//$('#approvalPosition').append($('<option>').text(value.userName+'/'+value.positionName).attr('value', value.positionId));  
+					$('#approvalPosition').append($('<option>').text(value.user.name).attr('value', value.assignments[0].position));  
 				});
 				$('#approvalPosition').val($('#approvalPositionValue').val());
 			}, 
@@ -103,6 +135,13 @@ $(document).ready(function()
 	});
 	
 	$('#approvalDepartment').trigger('change');
+	
+	$('#approvalPosition').change(function(){
+		
+		$("#approverName").val($('#approvalPosition option:selected').text());
+	});
+	
+	
 });
 
 function callAlertForDepartment() {
